a8eedf84d8cc2027633307c3e115fbf2498d9378
thousfeet <848804259@qq.com>
2016-06-07 16:47:06 +0800

    Update Scan.cpp

--- Calculator/Calculator/Scan.cpp
+++ Calculator/Calculator/Scan.cpp
@@ -1,4 +1,3 @@
-<<<<<<< 65557f4a3f3a059bbeefb8e4aa81534f9fbf8ba2
  /************************************************************
   FileName: Print.cpp
   Author: thousfeet
@@ -7,16 +6,6 @@
    	 queue<string> ToStringQueue(string input);
 	 bool check(string s);
  ***********************************************************/
-=======
-/************************************************************
- FileName: Print.cpp
- Author: thousfeet
- Date: 2016.03.23
- Function List:
-    queue<string> ToStringQueue(string input);
-    bool check(string s);
-***********************************************************/
->>>>>>> ver_3.0
 #include<iostream>
 #include<string>
 #include<cctype>
@@ -26,13 +15,12 @@ using namespace std;
 
 
 /*************************************************
-<<<<<<< 65557f4a3f3a059bbeefb8e4aa81534f9fbf8ba2
   Description:
-     ¼ì²éÊäÈëµÄÊı×Ö¸öÊı
-  Input: String s ±»¼ì²éµÄ×Ö·û´®
-  Output: ÎŞ
-  Return: Èô×Ö·û´®ÖĞÊı×Ö³¬¹ı10Î»£¬·µ»Øtrue£»ÈôÎ´³¬¹ı
-     Ôò·µ»Øfalse
+     æ£€æŸ¥è¾“å…¥çš„æ•°å­—ä¸ªæ•°
+  Input: String s è¢«æ£€æŸ¥çš„å­—ç¬¦ä¸²
+  Output: æ— 
+  Return: è‹¥å­—ç¬¦ä¸²ä¸­æ•°å­—è¶…è¿‡10ä½ï¼Œè¿”å›trueï¼›è‹¥æœªè¶…è¿‡
+     åˆ™è¿”å›false
 *************************************************/
 bool Scan::check(string s)
 {
@@ -42,13 +30,13 @@ bool Scan::check(string s)
 	for(int i = 0; i < s.size(); i++)
 	{
 
-		//ÈôÎª²»ÎªĞ¡ÊıµãµÄ·ÇÊı×Ö×Ö·û
+		//è‹¥ä¸ºä¸ä¸ºå°æ•°ç‚¹çš„éæ•°å­—å­—ç¬¦
 		if( !isdigit(s[i]) && s[i] != '.')
 		{
 			num = 0;
 		}
 
-		//ÈôÎªÊı×Ö×Ö·û»òĞ¡Êıµã
+		//è‹¥ä¸ºæ•°å­—å­—ç¬¦æˆ–å°æ•°ç‚¹
 		else if( isdigit(s[i]) || '.')
 		{
 			num++;
@@ -58,13 +46,13 @@ bool Scan::check(string s)
 				flag = true;
 			}
 
-			//ÓĞĞ¡ÊıÎ»
+			//æœ‰å°æ•°ä½
 			if(num > 11 && flag)
 			{
 				return true;
 			}
 
-			//ÎŞĞ¡ÊıÎ»
+			//æ— å°æ•°ä½
 			else if(num > 10 && !flag)
 			{
 				return true;
@@ -77,32 +65,32 @@ bool Scan::check(string s)
 
 /*************************************************
   Description:
-     ½ÓÊÕÒ»¸öËÄÔòÔËËã±í´ïÊ½£¨¼´input£©£¬Öğ¸ö×Ö·ûÉ¨ÃèÕâ
-     ¸ö±í´ïÊ½£¬½«Êı×ÖºÍ·ûºÅÌáÈ¡³öÀ´£¬µÃµ½Ò»×éstring
-  Input: String input ±»É¨ÃèµÄ×Ö·û´®
-  Output: ÎŞ
-  Return: queue<string> ×ª»»Ö®ºóµÄstring
+     æ¥æ”¶ä¸€ä¸ªå››åˆ™è¿ç®—è¡¨è¾¾å¼ï¼ˆå³inputï¼‰ï¼Œé€ä¸ªå­—ç¬¦æ‰«æè¿™
+     ä¸ªè¡¨è¾¾å¼ï¼Œå°†æ•°å­—å’Œç¬¦å·æå–å‡ºæ¥ï¼Œå¾—åˆ°ä¸€ç»„string
+  Input: String input è¢«æ‰«æçš„å­—ç¬¦ä¸²
+  Output: æ— 
+  Return: queue<string> è½¬æ¢ä¹‹åçš„string
 *************************************************/
 queue<string> Scan::ToStringQueue(string input)
 {
-	//³õÊ¼»¯
+	//åˆå§‹åŒ–
 	m_sInput = input;
 	while(StrQueue.size())
 		StrQueue.pop();
 
-	//±éÀúinputµÄÃ¿¸ö×Ö·û
+	//éå†inputçš„æ¯ä¸ªå­—ç¬¦
 	for(int i = 0; i  <m_sInput.size(); i++)
 	{
-		 //ÈôÎª²»ÎªĞ¡ÊıµãµÄ·ÇÊı×Ö×Ö·û
+		 //è‹¥ä¸ºä¸ä¸ºå°æ•°ç‚¹çš„éæ•°å­—å­—ç¬¦
 		if( !isdigit(m_sInput[i]) && m_sInput[i] != '.')
 		{
 			if(!temp.empty())
 			{
-				StrQueue.push(temp);  //Ôİ´æ×Ö·û´®·Ç¿ÕÔòÈë¶Ó
+				StrQueue.push(temp);  //æš‚å­˜å­—ç¬¦ä¸²éç©ºåˆ™å…¥é˜Ÿ
 			}
-			temp.clear();  //Èë¶ÓºóÇå³ıÔİ´æ×Ö·û´®
+			temp.clear();  //å…¥é˜Ÿåæ¸…é™¤æš‚å­˜å­—ç¬¦ä¸²
 
-			temp = m_sInput[i];   //µ±Ç°·ûºÅÈë¶Ó
+			temp = m_sInput[i];   //å½“å‰ç¬¦å·å…¥é˜Ÿ
 			if(!temp.empty())
 			{
 				StrQueue.push(temp);
@@ -110,16 +98,16 @@ queue<string> Scan::ToStringQueue(string input)
 			temp.clear();
 		}
 
-		//ÈôÎªÊı×Ö×Ö·û»òĞ¡Êıµã
+		//è‹¥ä¸ºæ•°å­—å­—ç¬¦æˆ–å°æ•°ç‚¹
 		else if( isdigit(m_sInput[i]) || '.')
 		{
-			temp += m_sInput[i];   //µ±Ç°Êı×Ö»òĞ¡Êıµã¸úÓÚÔİ´æ×Ö·û´®Ä©
+			temp += m_sInput[i];   //å½“å‰æ•°å­—æˆ–å°æ•°ç‚¹è·Ÿäºæš‚å­˜å­—ç¬¦ä¸²æœ«
 			continue;
 		}
 
 	}
 
-	//±éÀúÍê³ÉºóÔİ´æ×Ö·û´®·Ç¿ÕÔòÈë¶Ó
+	//éå†å®Œæˆåæš‚å­˜å­—ç¬¦ä¸²éç©ºåˆ™å…¥é˜Ÿ
 	if(!temp.empty())
 	{
 		StrQueue.push(temp);
@@ -127,110 +115,5 @@ queue<string> Scan::ToStringQueue(string input)
 
 	temp.clear();
 
-	return StrQueue;  //·µ»Ø´Ë¶ÓÁĞ
+	return StrQueue;  //è¿”å›æ­¤é˜Ÿåˆ—
 }
-
-=======
- Description:
-    æ£€æŸ¥è¾“å…¥çš„æ•°å­—ä¸ªæ•°
- Input: String s è¢«æ£€æŸ¥çš„å­—ç¬¦ä¸²
- Output: æ— 
- Return: è‹¥å­—ç¬¦ä¸²ä¸­æ•°å­—è¶…è¿‡10ä½ï¼Œè¿”å›trueï¼›è‹¥æœªè¶…è¿‡
-    åˆ™è¿”å›false
-*************************************************/
-bool Scan::check(string s)
-{
-   int num = 0;
-   bool flag = false;
-
-   for(int i = 0; i < s.size(); i++)
-   {
-
-       //è‹¥ä¸ºä¸ä¸ºå°æ•°ç‚¹çš„éæ•°å­—å­—ç¬¦
-       if( !isdigit(s[i]) && s[i] != '.')
-       {
-           num = 0;
-       }
-
-       //è‹¥ä¸ºæ•°å­—å­—ç¬¦æˆ–å°æ•°ç‚¹
-       else if( isdigit(s[i]) || '.')
-       {
-           num++;
-
-           if(s[i] == '.')
-           {
-               flag = true;
-           }
-
-           //æœ‰å°æ•°ä½
-           if(num > 11 && flag)
-           {
-               return true;
-           }
-
-           //æ— å°æ•°ä½
-           else if(num > 10 && !flag)
-           {
-               return true;
-           }
-       }
-
-   }
-   return false;
-}
-
-/*************************************************
- Description:
-    æ¥æ”¶ä¸€ä¸ªå››åˆ™è¿ç®—è¡¨è¾¾å¼ï¼ˆå³inputï¼‰ï¼Œé€ä¸ªå­—ç¬¦æ‰«æè¿™
-    ä¸ªè¡¨è¾¾å¼ï¼Œå°†æ•°å­—å’Œç¬¦å·æå–å‡ºæ¥ï¼Œå¾—åˆ°ä¸€ç»„string
- Input: String input è¢«æ‰«æçš„å­—ç¬¦ä¸²
- Output: æ— 
- Return: queue<string> è½¬æ¢ä¹‹åçš„string
-*************************************************/
-queue<string> Scan::ToStringQueue(string input)
-{
-   //åˆå§‹åŒ–
-   m_sInput = input;
-   while(StrQueue.size())
-       StrQueue.pop();
-
-   //éå†inputçš„æ¯ä¸ªå­—ç¬¦
-   for(int i = 0; i  <m_sInput.size(); i++)
-   {
-        //è‹¥ä¸ºä¸ä¸ºå°æ•°ç‚¹çš„éæ•°å­—å­—ç¬¦
-       if( !isdigit(m_sInput[i]) && m_sInput[i] != '.')
-       {
-           if(!temp.empty())
-           {
-               StrQueue.push(temp);  //æš‚å­˜å­—ç¬¦ä¸²éç©ºåˆ™å…¥é˜Ÿ
-           }
-           temp.clear();  //å…¥é˜Ÿåæ¸…é™¤æš‚å­˜å­—ç¬¦ä¸²
-
-           temp = m_sInput[i];   //å½“å‰ç¬¦å·å…¥é˜Ÿ
-           if(!temp.empty())
-           {
-               StrQueue.push(temp);
-           }
-           temp.clear();
-       }
-
-       //è‹¥ä¸ºæ•°å­—å­—ç¬¦æˆ–å°æ•°ç‚¹
-       else if( isdigit(m_sInput[i]) || '.')
-       {
-           temp += m_sInput[i];   //å½“å‰æ•°å­—æˆ–å°æ•°ç‚¹è·Ÿäºæš‚å­˜å­—ç¬¦ä¸²æœ«
-           continue;
-       }
-
-   }
-
-   //éå†å®Œæˆåæš‚å­˜å­—ç¬¦ä¸²éç©ºåˆ™å…¥é˜Ÿ
-   if(!temp.empty())
-   {
-       StrQueue.push(temp);
-   }
-
-   temp.clear();
-
-   return StrQueue;  //è¿”å›æ­¤é˜Ÿåˆ—
-}
->>>>>>> ver_3.0
